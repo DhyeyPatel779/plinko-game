f3d8375a77c0a253b34204836c6b5084e4e98cb1
DhyeyPatel779 <71546105+DhyeyPatel779@users.noreply.github.com>
2020-11-24 20:20:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,17 +7,49 @@ const Constraint = Matter.Constraint;
 var engine,world;
 var ground;
 
+var particles = [];
+var plinkos = [];
+var divisions = [];
 
+var divisionHeight = 300;
 
 function setup() {
   createCanvas(480,800);
   engine = Engine.create();
   world = engine.world;
 
-  createSprite(400, 200, 50, 50);
+  background.shapeColor = "black"
+
+  ground = new Ground(240,780,480,20)
 
 
-  ground = new Ground(480,20)
+for(var k= 0;k<=width; k = k+80){
+  divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight))
+}
+
+for(var j = 40; j<=width; j = j+50){
+  plinkos.push(new Plinko(j,75,10))
+}
+
+for(var j = 15; j<=width-10; j = j+50){
+  plinkos.push(new Plinko(j,175,10))
+}
+
+
+if(frameCount%60===0){
+particles.push(new Particle(random(width/2-10),10,10))
+
+for(var j = 0;j< particles.length; j++){
+  particles[j].display();
+}
+
+for(var k = 0;k< particles.length; k++){
+  particles[k].display();
+}
+
+
+
+}
   Engine.run(engine);
 }
 
