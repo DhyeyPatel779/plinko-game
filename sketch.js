195219b5d6b50518e4048118db34ef346fde611f
DhyeyPatel779 <71546105+DhyeyPatel779@users.noreply.github.com>
2020-11-25 11:58:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,68 +5,75 @@ const Constraint = Matter.Constraint;
 
 
 var engine,world;
-var ground;
 
+var ground;
 var particles = [];
 var plinkos = [];
 var divisions = [];
 
-var divisionHeight = 300;
+var divisionHeight=300;
+
+
 
 function setup() {
-  createCanvas(480,800);
+  createCanvas(800, 800);
   engine = Engine.create();
   world = engine.world;
-
-  background.shapeColor = "black"
-
-  ground = new Ground(240,780,480,20)
+  ground = new Ground(width/2,height,width,20);
 
 
-for(var k= 0;k<=width; k = k+80){
-  divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight))
+
+  for(var k= 0;k<=width; k = k+80){
+    divisions.push(new Division(k,height-divisionHeight/2,10,divisionHeight))
+  }
+
+
+    for (var j = 75; j <=width; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,75));
+    }
+
+    for (var j = 50; j <=width-10; j=j+50) 
+    {
+    
+       plinkos.push(new Plinko(j,175));
+    }
+
+ 
+
+
+    
+
+    
 }
-
-for(var j = 40; j<=width; j = j+50){
-  plinkos.push(new Plinko(j,75,10))
-}
-
-for(var j = 15; j<=width-10; j = j+50){
-  plinkos.push(new Plinko(j,175,10))
-}
-
-
-Engine.run(engine);
-}
-
+ 
 
 
 function draw() {
-  background(255,255,255);  
+  background("black");
+  textSize(20)
 
+  Engine.update(engine);
+ 
   
+  for (var i = 0; i < plinkos.length; i++) {
+     
+    plinkos[i].display();
+    
+  }
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-30, width/2+30), 10,10));
+    
+  }
 
-
-ground.display();
-
-
-if(frameCount%60===0){
-  particles.push(new Particle(random(width/2-10),10,10))
+ for (var j = 0; j < particles.length; j++) {
   
-  for(var j = 0;j< particles.length; j++){
     particles[j].display();
   }
-  
-  for(var k = 0;k< particles.length; k++){
-    particles[k].display();
+  for (var k = 0; k < divisions.length; k++) {
+    
+    divisions[k].display();
   }
-  
-  
-
-
-
-
-Engine.update(engine);
-  drawSprites();
 }
-}
+
